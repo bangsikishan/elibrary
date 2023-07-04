6616946bed43da02f6afae193427f535cc9b4a6b
Kishan <bangsikishan@duck.com>
2023-07-04 17:50:57 +0545

    Multer configurations

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 
 const bookRoute = require('./routes/BookRoute');
 
 const app = express();
+
+const storage = multer.diskStorage({
+    destination: './uploads',
+    filename: function(req, file, cb) {
+        const newFilename = file.filename + '-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFilename);
+    }
+});
+
+function checkFileType(file, cb) {
+    const allowedFileType = /pdf/;
+    const extname = allowedFileType.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedFileType.test(file.mimetype);
+
+    if (mimetype && extname) {
+        cb(null, true);
+    } else {
+        cb('Error: PDF files only!');
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    fileFilter: async function(req, file, cb) {
+    try {
+        await checkFileType(file);
+        cb(null, true);
+    } catch (err) {
+        cb(err);
+    }
+}
+});
 
 app.use(cors());
 app.use(express.json({ extended: true }));
