7d05b4a7ff7c8cbd9b294ee24395f40af3403d0f
Kishan <bangsikishan@duck.com>
2023-07-04 11:53:20 +0545

    Added fetch to send data

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -4,10 +4,24 @@ import { Input, Button } from "@material-tailwind/react";
 const Form = () => {
     const [book, setBook] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         
-        console.log(book);
+        const sanitizedInput = {
+            book: book.trim()
+        };
+
+        const response = await fetch('http://localhost:3000/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(sanitizedInput)
+        });
+
+        const data = await response.json();
+
+        console.log(data);
     }
 
     return (
