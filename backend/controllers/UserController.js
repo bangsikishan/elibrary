e32c807ae128984230d1c994aa404064760eaa12
Kishan <bangsikishan@duck.com>
2023-07-07 18:37:53 +0545

    Added signup functionality

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 const User = require('../models/User');
 
 const loginUser = async (req, res) => {
@@ -9,7 +11,34 @@ const loginUser = async (req, res) => {
 const signupUser = async (req, res) => {
     const { username, email, password } = req.body;
 
-    res.json({ message: 'Register User', username, email, password });
+    if (!username || !email || !password) {
+        return res.status(400).json({ error: 'Please enter all fields!' });
+    }
+
+    if (username.length < 3) {
+        return res.status(400).json({ error: 'Username must be at least 3 characters!' });
+    }
+
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ error: 'Please enter a valid email address!' });
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        return res.status(400).json({ error: 'Please enter a strong password!' });
+    }
+
+    const userExists = await User.findOne({ $or: [{ username: username }, { email: email }] });
+
+    if (userExists) {
+        return res.status(400).json({ error: 'User already exists!' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ username, email, password: hashedPassword });
+
+    res.status(201).json({ id: user._id, username: user.username });
 }
 
 module.exports = {
