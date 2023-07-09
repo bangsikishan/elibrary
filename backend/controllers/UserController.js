10f3a3f6bf3b3d6d81701a16c24fc3501659634a
Kishan <bangsikishan@duck.com>
2023-07-09 11:23:53 +0545

    Added login & jwt token functionality

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,11 +1,38 @@
 const bcrypt = require('bcrypt');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+}
 
 const loginUser = async (req, res) => {
     const { username, password } = req.body;
 
-    res.json({ message: 'Login User', username, password });
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Please enter all fields!' });
+    }
+
+    if (username.length < 3) {
+        return res.status(400).json({ error: 'Username must be at least 3 characters!' });
+    }
+
+    const userExists = await User.findOne({ username });
+
+    if (!userExists) {
+        return res.status(400).json({ error: 'User does not exist!' });
+    }
+
+    const hashedPassword = await bcrypt.compare(password, userExists.password);
+
+    if (!hashedPassword) {
+        return res.status(400).json({ error: 'Incorrect password!' });
+    }
+
+    const token = createToken(userExists._id);
+
+    res.status(201).json({ username: userExists.username, token });
 }
 
 const signupUser = async (req, res) => {
