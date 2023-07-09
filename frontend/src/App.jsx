48cd17a57c891a44fca9f2b4cd1886efbd882db7
Kishan <bangsikishan@duck.com>
2023-07-09 18:24:51 +0545

    Added route to add books

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import {
   createBrowserRouter, 
   createRoutesFromElements, 
   Route, 
-  RouterProvider 
+  RouterProvider
 } from "react-router-dom";
 
 import RootLayout from "./pages/RootLayout";
@@ -10,11 +10,13 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import AddBook from "./pages/AddBook";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path="add-book" element={<AddBook />} />
       <Route path="about" element={<About />} />
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<Signup />} />
