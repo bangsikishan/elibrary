77767b791bdb74b40daef929ff8761944843ff65
Kishan <bangsikishan@duck.com>
2023-07-13 13:37:43 +0545

    Added code to upload book

--- backend/controllers/BookController.js
+++ backend/controllers/BookController.js
@@ -1,14 +1,21 @@
 const Book = require('../models/Book');
-const { newFilename } = require('../config/multerConfig');
 
 const getBook = (req, res) => {
     console.log(req.body);
     res.json({ message: 'Get Book' });
 };
 
-const addBook = (req, res) => {
-    console.log(req.body, newFilename);
-    res.json({ message: 'Add Book' });
+const addBook = async (req, res) => {
+    const newFileName = req.file.filename;
+    const { book, author } = req.body;
+
+    if(!book || !author) {
+        return res.status(400).json({ error: 'Please enter all fields!' });
+    }
+
+    const bookData = await Book.create({ title: book, author, path: newFileName });
+    
+    res.status(201).json(bookData);
 };
 
 module.exports = {
