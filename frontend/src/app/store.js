862f38f0f7011183875b7af3c06d247d795499ba
Kishan <bangsikishan@duck.com>
2023-07-23 18:55:13 +0545

    Added functionality to get username & token from localstorage

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,9 +2,20 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '../features/auth/authSlice';
 
+const storedUserToken = localStorage.getItem('user');
+const storedUsername = localStorage.getItem('username');
+
+const initialAuthState = {
+    userName: storedUsername,
+    user: storedUserToken,
+  };
+
 const store = configureStore({
     reducer: {
         auth: authReducer
+    },
+    preloadedState: {
+        auth: initialAuthState
     }
 });
 
